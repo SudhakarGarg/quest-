e7ae3058939bf1db568d405e312b473af22eedd1
sudhakar garg <krishugarg@Sudhakars-MBP-M2.local>
2024-06-14 02:53:48 +0530

    jai mata di

--- app.js
+++ app.js
@@ -3,63 +3,39 @@ const morgan = require('morgan');
 const mongoose = require('mongoose');
 const blogroutes = require('./routes/blogroutes');
 const app = express();
-//connect to mongodb
-const dbURI='mongodb+srv://sudhakargarg2193:Home3141@cluster0.es6paa6.mongodb.net/node-tuts?retryWrites=true&w=majority&appName=Cluster0';
-mongoose.connect(dbURI ,{useNewUrlParser:true,useUnifiedTopology:true})
-.then((result)=> app.listen(3000))
-.catch((err)=> console.log(err));
+
+// Connect to MongoDB
+const dbURI = 'mongodb+srv://sudhakargarg2193:Home3141@cluster0.es6paa6.mongodb.net/node-tuts?retryWrites=true&w=majority&appName=Cluster0';
+mongoose.connect(dbURI, { useUnifiedTopology: true, useNewUrlParser: true })
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err));
 
 // Register view engine
 app.set('view engine', 'ejs');
 
-// Start the server
-//app.listen(3000);
-
-//middleware & static files
+// Middleware & static files
 app.use(express.static('public'));
-app.use(express.urlencoded({extended: true}));
-
-//--------------------------------------------------------------------
-/*
-app.use((req,res,next)=>
-{
-   console.log('new request made:');
-   console.log('host:',req.hostname);
-   console.log('path:',req.path);
-   console.log('method:',req.method);
-   next();
-});
-app.use((req,res,next)=>
-{
-   console.log('in the next middleware:');
-   next();
-});
-*/
-
+app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
-//----------------------------------------------------------------------
-
-
 
 // Routes
 app.get('/', (req, res) => {
-  res.redirect('/blogs')
+  res.redirect('/blogs');
 });
 
 app.get('/about', (req, res) => {
-    res.render('about',{title:'About'}); // Renders the 'about.ejs' view
+  res.render('about', { title: 'About' });
 });
 
-// blog routes
-app.use('/blogs',blogroutes);
-
+// Blog routes
+app.use('/blogs', blogroutes);
 
 // Redirects
 app.get('/about-us', (req, res) => {
-    res.redirect('/about'); // Redirects /about-us to /about
+  res.redirect('/about');
 });
 
 // 404 Page
 app.use((req, res) => {
-    res.status(404).render('404',{title:'404'}); // Renders the '404.ejs' view with a 404 status
+  res.status(404).render('404', { title: '404' });
 });
